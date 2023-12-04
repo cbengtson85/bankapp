13afbd5cb132b35a8bbca7d414509a93986d3419
Craig Bengtson <cbengtson85@gmail.com>
2023-12-03 19:08:48 -0600

    Update App.jsx

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -3,14 +3,12 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import {Home} from 'app/components/Home';
 import {AccountDetails} from 'app/components/AccountDetails';
-import {HeaderContainer} from 'app/components/Common';
 
 class App extends React.Component {
     render() {
         return (
             <BrowserRouter>
                 <div>
-                    <Route path="*" component={HeaderContainer} />
                     <Switch>
                         <Route exact path="/" component={Home} />
                         <Route path="/:page" component={AccountDetails} />
