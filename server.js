52710728eef8699ae8ee66307b6f24e29c0bd75c
Craig Bengtson <cbengtson85@gmail.com>
2024-01-07 14:27:18 -0600

    Update server.js

--- server.js
+++ server.js
@@ -18,12 +18,65 @@ app.use(express.static('static'));
 app.use(express.static('pics'));
 
 app.get('/pics', (req, res) => {
-    const arr = [];
-    fs.readdirSync(path.resolve('./pics')).forEach((file) => {
-        arr.push(file)
-    })
+    const filesObj = [];
+
+    const getDate = (fileName) => {
+        try {
+            const dateStr = fileName.split('_')[0]
+            
+            const year   = dateStr.substring(0, 4);
+            const month  = dateStr.substring(4, 6);
+            const day    = dateStr.substring(6, 8);
+            
+            const abc = Date.parse(year+ '-' + month + '-' + day);
+            
+            return (new Date(abc)).toDateString()
+        } catch (error) {
+            return ''
+        }
+    }
+
+    const getFilesRecursively = (directory) => {
+        const filesInDirectory = fs.readdirSync(directory);
+        for (const file of filesInDirectory) {
+            const absolute = path.join(directory, file);
+            if (fs.statSync(absolute).isDirectory()) {
+                getFilesRecursively(absolute);
+            } else {
+                if(file.indexOf('.DS_Store') < 0) {
+
+                    let filepath = absolute.split('/pics/')[1];
+                    if(!filepath) {
+                        filepath = absolute.split('\\pics\\')[1];
+                    }
+                    let fileArrSplit = filepath.split('/');
+                    if(!fileArrSplit) {
+                        fileArrSplit = filepath.split('\\');
+                    }
+                    let loc1 = fileArrSplit[fileArrSplit.length - 2]
+                    let loc2 = fileArrSplit[fileArrSplit.length - 3]
+
+                    if(!loc2) {
+                        loc1 = loc1.slice(0,-5)
+                    } else {
+                        loc2 = loc2.slice(0, -5)
+                    }
+
+                    const fileObj = {
+                        src: absolute.split('/pics/')[1],
+                        date: getDate(file),
+                        location1: loc1,
+                        location2: loc2
+                    }
+                    filesObj.push(fileObj);
+                } 
+            }
+        }
+    };
+    getFilesRecursively(path.resolve('./pics'))
+
     res.json({
-        files: lodash.shuffle(lodash.shuffle(arr))
+        files: lodash.shuffle(lodash.shuffle(lodash.shuffle(filesObj)))
     })
 })
 
